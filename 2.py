1029fb48b620ab10620fd7c9cd15c29cf470c80e
Devrim <kocak.devrim@hotmail.com>
2026-03-26 12:46:15 +0100

    A

---
+++ 2.py
@@ -0,0 +1,3 @@
+def multiply_numbers(a, b):
+    return a * b
+
--- First Git.py
+++ First Git.py
@@ -2,3 +2,10 @@ HELLO WORLD
 print("HELLO WORLD")
 print("HELLO WORLD")
 print("HELLO WORLD")
+
+def divide_numbers(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
+
